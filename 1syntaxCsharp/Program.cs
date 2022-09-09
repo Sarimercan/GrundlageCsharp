e52e54da07c4d2337c4ca8e4b886a6ea5854827d
Okan Sarimercan <okan.s7@gso.schule.koeln>
2022-09-09 13:48:41 +0200

    Aufgabe 2

--- 1syntaxCsharp/Program.cs
+++ 1syntaxCsharp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("_-_")
+﻿// Aufgabe 2
+
+
+string alter = "19";
+string name = "Okan";
+string nachname = "Sarimercan";
+
+Console.WriteLine(alter + " " + name + " " + nachname);
+
